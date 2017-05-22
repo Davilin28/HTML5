ca1709e7bcc98e8a9d959f92517c63eab018de01
Davilin28 <davidromanrey28@gmail.com>
2017-05-22 12:40:49 +0200

    Recreativa

---
+++ recreativa/js/recrejs.js
@@ -0,0 +1,25 @@
+function play() {
+
+}
+function save() {
+	
+}
+function restart() {
+
+	var p;
+
+	p = document.getElementById("result1");
+	p.innerHTML = "0";
+
+	p = document.getElementById("result2");
+	p.innerHTML = "0";
+}
+function change1() {
+
+}
+function change2() {
+
+}
+function change3() {
+
+}
