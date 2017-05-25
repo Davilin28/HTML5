38853e76eec4868560b91153bb9715729a429ae7
Davilin28 <davidromanrey28@gmail.com>
2017-05-25 09:56:17 +0200

    HTML

--- Recreativa final/js/recrejs.js
+++ Recreativa final/js/recrejs.js
@@ -22,8 +22,7 @@ function CambiarImagen(idImagen) {
 	/* idImagen --> Indica la imagen que se quiere cambiar. 
 	El valor al ser un id habra que llamarlo con comillas simples '	'*/
 
-	var numero,
-		imagen;
+	var numero, imagen;
 
 	imagen = document.getElementById(idImagen);	
 	numero = Math.floor((Math.random() * 3) + 1);
@@ -35,4 +34,39 @@ function CambiarImagen(idImagen) {
 			imagen.src = "../imagenes/Fresa.png"
 		else
 			imagen.src = "../imagenes/Limon.png"
+}
+function CalcularPuntuacion() {
+	var imagen1, imagen2, imagen3, puntos, tirActual;
+
+	imagen1 = document.getElementById('imagen1');
+	imagen2 = document.getElementById('imagen2');
+	imagen3 = document.getElementById('imagen3');
+
+	if ((imagen1.src == imagen2.src) && (imagen2.src == imagen3.src))
+	// las tres imagenes son iguales
+	puntos = 1000;
+	else
+		if ( (imagen1.src == imagen2.src) ||
+		    (imagen1.src == imagen3.src) ||
+		    (imagen2.src == imagen3.src) )
+	// Dos imagenes iguales
+	puntos = 500;
+	else
+	// Las tres imagenes distintas.
+	puntos = 0
+
+	// Actualizar el contador actual
+	tirActual = document.getElementById('TiradaActual');
+	tirActual.innerHTML = puntos;
+}
+function Jugar() {
+
+	CambiarImagen('imagen1');
+	CambiarImagen('imagen2');
+	CambiarImagen('imagen3');
+	CalcularPuntuacion();
+}
+function CambiarPuntuar(idImagen) {
+	CambiarImagen(idImagen);
+	CalcularPuntuacion();
 }
